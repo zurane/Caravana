a10c28a51297feb7b3a08d92384e10bce6004072
zurane <giftedmpho99@gmail.com>
2023-07-12 20:27:57 +0200

    fetching data and display on vans page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -3,10 +3,30 @@ import { ReactDOM } from "react";
 
 
 export default function () {
+
+    React.useEffect(() => {
+
+        fetch("/api/vans").then(res => res.json()).then(data => console.log(data));
+    })
     
     return (
-        
-        <h1>Caravans page</h1>
-        
+        <>
+        <h1>Explore our caravan options</h1>
+        <div className="tabs">
+            <span>Simple</span>
+            <span>Luxury</span>
+            <span>Rugged</span>
+        </div>
+        <section className="cards">
+            
+            <div className="cards-row">
+
+                <div className="card">
+                    
+                </div>
+
+            </div>
+            </section> 
+        </>       
     );
 }
