1e01cd4dec834143af7b1b213ed33ffaee5aef25
zurane <giftedmpho99@gmail.com>
2023-07-15 11:36:00 +0200

    userParams method import on the VanDetail page

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
+  const params = useParams();
+  console.log(params);
   return <h1>Van Detail page</h1>;
 }
