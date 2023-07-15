1dcc3fc7fd3879ec0cbd4791a1e87241620334e7
zurane <giftedmpho99@gmail.com>
2023-07-15 16:39:46 +0200

    Version 2: Fetch data from server.

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -4,7 +4,9 @@ import { useParams } from "react-router-dom";
 export default function VanDetail() {
   const params = useParams();
   React.useEffect(() => {
-    fetch(`/api/vans/${params.id}`).then((data) => console.log(data));
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }, [params.id]);
   return <h1>Van Detail page</h1>;
 }
