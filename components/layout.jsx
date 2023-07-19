5ee82ffc78f4a1422075791df14240f43f7d5a2b
zurane <giftedmpho99@gmail.com>
2023-07-19 23:57:41 +0200

    Work in progress.

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./pages/Navbar";
 export default function Layout() {
   return (
     <>
       <h1>This is a layout</h1>
+      <Navbar />
       <Outlet />
     </>
   );
