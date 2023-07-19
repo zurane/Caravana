477a176facadeb6017b3a4a2a39e070cf72fb3f6
zurane <giftedmpho99@gmail.com>
2023-07-20 00:59:08 +0200

    A new Build test.

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Navbar from "/pages/Navbar";
-import Footer from "/pages/Footer";
+import Navbar from "./pages/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <Navbar />
       <Outlet />
-      <Footer />
     </>
   );
 }
