e2493bf7b5abbf2c419a6fad3868fb3e6fc67f7d
zurane <giftedmpho99@gmail.com>
2023-07-20 00:40:24 +0200

    Fresh Build and Layout Router added

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
