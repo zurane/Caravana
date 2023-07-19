8d2e09fa841b401d47f358420415a58561d36a2e
zurane <giftedmpho99@gmail.com>
2023-07-19 23:46:31 +0200

    New components added.

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,8 +6,8 @@ import Footer from "./pages/Footer";
 export default function Layout() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <Navbar />,
+      <Outlet />,
       <Footer />
     </>
   );
