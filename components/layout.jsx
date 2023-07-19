cb7f7cf7ff4427165b75ed988adfcb578b0cf31c
zurane <giftedmpho99@gmail.com>
2023-07-20 00:16:31 +0200

    New Build & Routes Update.

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./pages/Navbar";
 
 export default function Layout() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Outlet />
     </>
   );
