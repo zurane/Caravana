b4c1365b7ad8b7ecef96ad2ec908be931b6e7222
zurane <giftedmpho99@gmail.com>
2023-07-20 00:19:36 +0200

    Updated Build Version.

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 import Navbar from "./pages/Navbar";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
