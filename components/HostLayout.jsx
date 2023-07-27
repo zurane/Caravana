e6c223d83ea7e6ea544670db3f00366b3b881342
zurane <giftedmpho99@gmail.com>
2023-07-27 20:43:43 +0200

    Missing imports added.

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function HostLayout() {
   return (
--- pages/host/Dashboard.jsx
+++ pages/host/Dashboard.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-
-export default function Income() {
+export default function Dashboard() {
   return (
     <>
       <h1>This is an Dashboard section</h1>
