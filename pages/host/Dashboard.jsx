aa0d7b266c44d421562b1e58d413402d25c99c41
zurane <giftedmpho99@gmail.com>
2023-07-27 03:50:11 +0200

    Dashboard outlet

--- pages/host/Dashboard.jsx
+++ pages/host/Dashboard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Income() {
-  return <h1>This is an Dashboard section</h1>;
+  return (
+    <>
+      <h1>This is an Dashboard section</h1>
+      <Outlet />
+    </>
+  );
 }
