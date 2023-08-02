3a32ad56acc3220f6f6399ab93859c836207efd0
zurane <giftedmpho99@gmail.com>
2023-08-02 06:02:09 +0200

    Host layout page cleaned up

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -2,26 +2,80 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
-
 export default function HostLayout() {
-
-  
   return (
     <>
-      <div className="dashboard__tabs">
-        <ul>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/income">Income</Link>
-          </li>
-          <li>
-            <Link to="/reviews">Reviews</Link>
-          </li>
-        </ul>
-      </div>
+      <Navigation />
       <Outlet />
     </>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Navigation() {
+  return (
+    <div className="dashboard__tabs">
+      <ul>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/income">Income</Link>
+        </li>
+        <li>
+          <Link to="/reviews">Reviews</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
